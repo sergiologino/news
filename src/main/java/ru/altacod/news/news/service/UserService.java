1dd4b359b1715b87cc7f4f0abb8a3c583199a59d
sergiologino <savkin.sergey@yandex.ru>
2024-03-06 08:21:15 +0300

    full model and repo Must add all controllers

--- src/main/java/ru/altacod/news/news/service/UserService.java
+++ src/main/java/ru/altacod/news/news/service/UserService.java
@@ -1,4 +1,15 @@
 package ru.altacod.news.news.service;
 
+import ru.altacod.news.news.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    List<User> findAll();
+
+    User findById (Long id);
+
+    User save(User user);
+
+    User update(User user);
 }
